56eceed01bb2aa006f951d40a896f3885457fa7a
Ani <ani@Safar-MacBook-Air.local>
2022-07-30 16:26:05 +0200

    basic react app created

---
+++ size_calculator/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Size calculator</h1>
+    <h2>Select a brand</h2>
+    <h2>Select a category</h2>
+    <p>My size is ... inches.</p>
+    <button>CALCULATE</button>
+    </>
+  );
+}
+
+export default App;
